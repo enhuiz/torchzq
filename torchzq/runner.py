6d03934f8152a97186969aba636090fb54e7a814
enhuiz <niuzhe.nz@outlook.com>
2020-06-27 15:17:44 +0800

    fix typo

--- torchzq/runner.py
+++ torchzq/runner.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 from pathlib import Path
 
 from torchzq import checkpoint
-from torchzq.logger import logger
+from torchzq.logger import Logger
 
 
 class Runner():
