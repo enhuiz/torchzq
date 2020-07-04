78d9ca0658c861d6ad415d3e35029457469a4b60
enhuiz <niuzhe.nz@outlook.com>
2020-07-04 14:02:02 +0800

    update epoch counter

--- torchzq/runner.py
+++ torchzq/runner.py
@@ -169,7 +169,7 @@ class Runner:
                 self.logger.log("lr", scheduler.get_last_lr()[0])
                 self.logger.log("loss", loss.item())
 
-                pbar.set_description(f"Epoch: {epoch}/{erange.stop}")
+                pbar.set_description(f"Epoch: {epoch}/{erange.stop - 1}")
                 for i, item in enumerate(self.logger.render(["step", "lr", "loss"])):
                     plines[i].set_postfix_str(item)
 
