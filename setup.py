56d07373d315458f0265b9a1d8e6efc1f9885e3f
enhuiz <niuzhe.nz@outlook.com>
2020-09-04 15:17:58 +0800

    support pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="torchzq",
     python_requires=">=3.6.0",
-    version="1.1",
+    version="1.0.1",
     description="TorchZQ: A simple PyTorch experiment runner.",
     author="enhuiz",
     author_email="niuzhe.nz@outlook.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=["torchzq", "torchzq/runners"],
     scripts=["zouqi", "zqboard"],
     install_requires=["torch", "tensorboardX", "tensorboard"],
+    url="https://github.com/enhuiz/torchzq",
 )
