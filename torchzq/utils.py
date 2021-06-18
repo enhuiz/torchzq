c623b78521e85085a50c263c409889bdbea262d8
enhuiz <niuzhe.nz@outlook.com>
2021-06-18 17:41:07 +0800

    add a helper for early stop

--- torchzq/utils.py
+++ torchzq/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+import torch
+import torch.nn as nn
 from pathlib import Path
 from natsort import natsorted
 
@@ -24,3 +27,22 @@ def default_tuple(x, default):
     for i in range(len(default) - len(x), len(default)):
         x.append(default[i])
     return tuple(x)
+
+
+class EarlyStop(nn.Module):
+    def __init__(self, patience):
+        super().__init__()
+        self.patience = patience
+        self.register_buffer("count", torch.zeros([]))
+        self.register_buffer("minima", torch.full([], np.inf))
+
+    def forward(self, x):
+        if x >= self.minima:
+            self.count += 1
+        else:
+            self.count = torch.zeros_like(self.count)
+        self.minima = torch.minimum(self.minima, x * torch.ones([]))
+        return (self.count >= self.patience).item()
+
+    def __str__(self):
+        return f"EarlyStop(count={self.count:.4g}, minima={self.minima:.4g})"
