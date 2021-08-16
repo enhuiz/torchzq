18dcac67dd72302f1279b273c00ea287acc10a7d
enhuiz <niuzhe.nz@outlook.com>
2021-08-16 20:44:30 +0800

    fix args with space in between

--- torchzq/cli.py
+++ torchzq/cli.py
@@ -9,11 +9,15 @@ def main():
     parser.add_argument("yaml", type=Path)
     parser.add_argument("command", type=str)
     args, manual_argv = parser.parse_known_args()
+
     data = load_yaml(args.yaml, args.command)
+
     if "runner" not in data:
         raise ValueError("Please specify a runner in your config file.")
+
     runner = data["runner"]
     del data["runner"]
+
     if "name" not in data and "--name" not in manual_argv:
         rpath = args.yaml.relative_to(".").with_suffix("")
         if str(rpath).startswith("..") or len(rpath.parts) < 2:
@@ -22,6 +26,11 @@ def main():
                 "Please make sure it is under a subdirectory of the current directory."
             )
         manual_argv.extend(["--name", str(Path(*rpath.parts[1:]))])
+
+    for i in range(len(manual_argv)):
+        if not manual_argv[i].startswith("--"):
+            manual_argv[i] = f"'{manual_argv[i]}'"
+
     cmd = f"{runner} {args.command} --config {args.yaml} {' '.join(manual_argv)} --config-ignored runner"
     os.system(cmd)
 
